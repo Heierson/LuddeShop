21da2f736c65ea44abba7d5a7546e1dcaf1917f5
Lukas Bohman <lukas-bohman@hotmail.com>
2021-07-09 14:00:48 +0200

    uppdaterat navbar med logotyp och skrivit produktbeskrivningar

--- src/main/java/com/example/LuddeShop/ProductRepository.java
+++ src/main/java/com/example/LuddeShop/ProductRepository.java
@@ -10,16 +10,16 @@ public class ProductRepository {
     private List<Product> products;
 
     public ProductRepository() {
-        products = new ArrayList<>();   //todo lägg in en kategorivariabel för att kunna sortera utbudet
+        products = new ArrayList<>();
         products.add(new Product(1001, "Barkus Krunegård", "En hipp jacka för alla coola jyckar där ute!", 1050, "Hundjackor"));
-        products.add(new Product(1002, "Floating Floof", "Flytväst", 589, "Hundjackor"));
-        products.add(new Product(1003, "Sausage Dog Ultra", "Dunjacka", 2200, "Hundjackor"));
-        products.add(new Product(1004, "Sir Barkington III", "Smoking för finare tillställningar", 850, "Hundjackor"));
-        products.add(new Product(1005, "Air Puppies", "Street skor för hund", 89, "Hundskor"));
+        products.add(new Product(1002, "Floating Floof", "Flyter din hund? Ta det säkra före det osäkra och klicka hem denna exklusiva och snygga flytväst.", 589, "Hundjackor"));
+        products.add(new Product(1003, "Sausage Dog Ultra", "Det perfekta plagget för dom lite kyligare promenaderna.", 2200, "Hundjackor"));
+        products.add(new Product(1004, "Sir Barkington III", "Smoking för finare tillställningar.", 850, "Hundjackor"));
+        products.add(new Product(1005, "Air Puppies", "En outfit är ingenting utan ett par schyssta kicks! Säkra looken med dessa stiliga skorna.", 89, "Hundskor"));
         products.add(new Product(1006, "Atomic Mongrel", "Med dessa löparskor på tassarna kommer din hund vara snabbast i kvarteret.", 150, "Hundskor"));
-        products.add(new Product(1007, "Cozy-Pup", "Hundsstrumpor", 975, "Hundskor"));
-        products.add(new Product(1008, "Furry Potatoe Satchel", "Modemedveten hundväska", 1500, "Hundväska"));
-        products.add(new Product(1009, "Bark Bark", "Hundväska", 3000, "Hundväska"));
+        products.add(new Product(1007, "Cozy-Pup", "Härliga strumpor i ullfrotté för regniga och kalla dagar när det är skönare att stanna inne framför brasan.", 975, "Hundskor"));
+        products.add(new Product(1008, "Furry Potatoe Satchel", "Om du har en lat hund är denna väska ett måste! Stor nog för både hund och leksaker.", 1500, "Hundväska"));
+        products.add(new Product(1009, "Bark Bag", "Modemedveten hundväska med fack för väsentligheter så som godis och leksaker.", 3000, "Hundväska"));
         products.add(new Product(1010, "Brandy Barrel", "Brandy barrel i klassisk stil - perfekt när törsten smyger sig på under promenaden.", 970, "Hundväska"));
     }
 
